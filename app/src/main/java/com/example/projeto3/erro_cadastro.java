4b17f61da39b18e8a2c1c84e4a05cf992485b597
Fernando Reinaldo <fernandoreinaldo30@outlook.com>
2023-05-02 23:07:23 -0300

    criacao de telas

---
+++ app/src/main/java/com/example/projeto3/erro_cadastro.java
@@ -0,0 +1,14 @@
+package com.example.projeto3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class erro_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erro_cadastro);
+    }
+}
